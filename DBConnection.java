d51c8f5d93fc3777a32b7c81ffeea07eba929163
Bill-Boucher <77949694+Bill-Boucher@users.noreply.github.com>
2021-04-12 20:11:06 -0600

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection  {
     private static final String URL = "jdbc:mysql://localhost:3306/Game_Character";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "rufus.188";
+    private static final String PASSWORD = "password";
     private  static  DBConnection INSTANCE = new DBConnection();
 
     private  Connection connection;
